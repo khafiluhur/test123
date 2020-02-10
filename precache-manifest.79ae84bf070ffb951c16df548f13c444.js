9b4646e4355025f56e7fda669c865f6248414aa9
khafiluhur <khafiluhur.99@gmail.com>
2020-02-10 07:36:30 +0000

    first commit

---
+++ precache-manifest.79ae84bf070ffb951c16df548f13c444.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "034f3a27886448e4a72d",
+    "url": "/frontend/css/app.34b2bbac.css"
+  },
+  {
+    "revision": "034f3a27886448e4a72d",
+    "url": "/frontend/js/app.a671ff2f.js"
+  },
+  {
+    "revision": "6771a6c96af275a61785",
+    "url": "/frontend/css/chunk-vendors.a3b9f02d.css"
+  },
+  {
+    "revision": "6771a6c96af275a61785",
+    "url": "/frontend/js/chunk-vendors.c37323e7.js"
+  },
+  {
+    "revision": "e951c4417bd8fb604a71",
+    "url": "/frontend/js/demo.a7c056c6.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/frontend/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/frontend/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/frontend/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/frontend/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/frontend/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "4c3075c9b0ad31df99fc923af3d91dc1",
+    "url": "/frontend/fonts/materialdesignicons-webfont.4c3075c9.woff2"
+  },
+  {
+    "revision": "88ffecafb926b79650b99451e298ee37",
+    "url": "/frontend/fonts/materialdesignicons-webfont.88ffecaf.woff"
+  },
+  {
+    "revision": "c7a79340c761ce19783b304cad1075a7",
+    "url": "/frontend/fonts/materialdesignicons-webfont.c7a79340.eot"
+  },
+  {
+    "revision": "2abd86f90b3fd9d18d950195282cd8d5",
+    "url": "/frontend/fonts/materialdesignicons-webfont.2abd86f9.ttf"
+  },
+  {
+    "revision": "bb3232102a53e0f9d86b048f1d4baa65",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "b6adc69e0f08b9401e9658e8f2988913",
+    "url": "/frontend/health.ico"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/frontend/img/brand/favicon.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/frontend/img/brand/white.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/frontend/img/brand/green.png"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/frontend/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/frontend/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/frontend/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/frontend/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/frontend/img/theme/vue.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/frontend/robots.txt"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/frontend/img/theme/angular.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/frontend/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/frontend/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/frontend/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "f5c035137056ad5f6b91bbb9f07444cb",
+    "url": "/frontend/img/brand/health.png"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/frontend/img/theme/team-4-800x800.jpg"
+  }
+];
